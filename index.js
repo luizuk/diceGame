0bb6fcae84585260f9ae3e2b1eb6c7091da07644
Luiz Paulo Silva Souza Pinto <35464652+luizuk@users.noreply.github.com>
2021-02-18 12:42:37 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1; //1-6
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0]; //index é o valor do elemento na ordem de todos os elementos img que existirem.
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 wins";
+}   else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "Player 2 wins";
+}   else if (randomNumber1 === randomNumber2) {
+        document.querySelector("h1").innerHTML = "Draw";
+}
